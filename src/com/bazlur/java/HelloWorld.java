9d33c51b63f7bda4470ac1d3ebe354f56acf33be
DaiyanNDahy <daiyanndahy@gmail.com>
2023-08-21 07:51:15 +0600

    uploading data

---
+++ src/com/bazlur/java/HelloWorld.java
@@ -0,0 +1,10 @@
+
+package com.bazlur.java;
+
+public class HelloWorld {
+
+    protected final static String hello="value";
+    public static void main(String[] args){
+        System.out.println("HelloWorld");
+    }
+}
